09c8443e717d279c30d285febe12563682d151a9
Hassan <90248143+hassan-jawdat@users.noreply.github.com>
2022-10-11 14:05:39 +0200

    Add files via upload

---
+++ test_shopping_cart.py
@@ -0,0 +1,25 @@
+from shopping_cart import ShoppingCart 
+import pytest
+
+def test_can_add_item_to_cart(): 
+    cart = ShoppingCart(5)
+    cart.add("apple")
+    assert cart.size() == 1    
+
+
+def test_when_item_added_then_cart_contains_item():
+    cart = ShoppingCart(5)
+    cart.add("apple")
+    assert "apple" in cart.get_items() 
+
+
+def test_when_add_more_than_max_items_should_fail():
+    cart = ShoppingCart(5)
+    for i in range(6):
+            cart.add("apple")
+    pass          
+ 
+ 
+
+
+
